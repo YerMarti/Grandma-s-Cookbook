ab3863fbdb39a962f3eb80259c63c97be02ec748
Yeray Martinez <yeray.martinez@opendeusto.es>
2024-12-30 11:39:17 +0100

    Fixed recipe load

--- recipes-api/services/recipe_service.py
+++ recipes-api/services/recipe_service.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from models.recipe import Recipe
 
@@ -21,20 +22,24 @@ def get_recipes_by_name(name: str) -> list[Recipe]:
     list[Recipe]
         A list of Recipe objects that match the query name.
     """
-    url = config.api_url + "search.php"
+    config.logger.debug(f"Searching recipes with name '{name}'...")
+    url = config.api_url + "/search.php"
 
     params = { "s": name }
 
     request = requests.get(url=url, params=params)
     request.raise_for_status()
 
-    meals = request.content["meals"]
+    res = json.loads(request.content.decode("utf-8"))
+    meals = res["meals"]
 
-    if meals is None:
+    if meals is None or len(meals) == 0:
+        config.logger.debug("Could not find any recipes.")
         return None
     
-    recipes = [Recipe(meal) for meal in meals]
+    recipes = [Recipe.from_data(meal) for meal in meals]
 
+    config.logger.debug(f"{len(recipes)} recipes found.")
     return recipes
 
 
@@ -52,20 +57,24 @@ def get_recipe_by_id(id: str) -> Recipe:
     Recipe
         A Recipe object with the requested id.
     """
-    url = config.api_url + "lookup.php"
+    config.logger.debug(f"Retrieving recipe with id '{id}'...")
+    url = config.api_url + "/lookup.php"
 
     params = { "i": id }
 
     request = requests.get(url=url, params=params)
     request.raise_for_status()
 
-    meal = request.content["meals"]
+    res = json.loads(request.content.decode("utf-8"))
+    meal = res["meals"]
 
-    if meal is None:
+    if meal is None or len(meal) == 0:
+        config.logger.debug("Recipe not found.")
         return None
     
-    recipe = Recipe(meal[0])
+    recipe = Recipe.from_data(meal[0])
 
+    config.logger.debug("Recipe retrieved.")
     return recipe
 
 
@@ -78,16 +87,20 @@ def get_random_recipe() -> Recipe:
     Recipe
         A Recipe object.
     """
-    url = config.api_url + "random.php"
+    config.logger.debug("Retrieving a random recipe...")
+    url = config.api_url + "/random.php"
 
     request = requests.get(url=url)
     request.raise_for_status()
 
-    meal = request.content["meals"]
+    res = json.loads(request.content.decode("utf-8"))
+    meal = res["meals"]
 
-    if meal is None:
+    if meal is None or len(meal) == 0:
+        config.logger.debug("Could not find a recipe.")
         return None
     
-    recipe = Recipe(meal[0])
+    recipe = Recipe.from_data(meal[0])
 
+    config.logger.debug("Random recipe retrieved.")
     return recipe
